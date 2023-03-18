339890ff8ae81b8e12e8ebdb6a2a4e0700d22a4e
remflood <m.aliraza.uol@gmail.com>
2023-03-19 00:05:55 +0500

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { useState, useRef, useEffect } from 'react';
 import './App.css';
+import Block from './comp/Block';
 
 function App() {
+
+  const one = 1;
+  const two = 2;
+  
+  let alreadyActive = false;
+
+  let obj_1 = {
+    id: 1,
+    isActive: false
+  }
+
+  let obj_2 = {
+    id: 2,
+    isActive: false
+  }
+
+  let blockIdList = [obj_1];
+
+  function order(id, isNear){
+
+    let value = false;
+
+    blockIdList.forEach(block => {
+      if(block.id === id){
+        if(isNear && !alreadyActive){
+          block.isActive = true;
+          alreadyActive = true;
+          value = true;
+        } else {
+          if(block.isActive && !isNear){
+            block.isActive = false;
+            alreadyActive = false;
+            value = true;
+          }
+        }
+      } else {
+        value = false;
+      }
+    });
+
+    return value;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Block id={one} func={order}/>
+      <Block id={one} func={order}/>
+      <Block id={one} func={order}/>
     </div>
   );
 }
---
+++ src/comp/Subject.js
@@ -0,0 +1,9 @@
+import './stylesheets/subject.css'
+
+export default function Subject(){
+    return (
+        <div className="subject">
+            Physics
+        </div>
+    );
+}
