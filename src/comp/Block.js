555f74d58b43d1d46a45ea3203561f7d8c3376fe
remflood <m.aliraza.uol@gmail.com>
2023-04-03 10:41:04 +0500

    save

--- src/comp/Block.js
+++ src/comp/Block.js
@@ -1,61 +1,57 @@
 import { useState, useRef, useEffect } from 'react';
 import './stylesheets/block.css'
 
-export default function Block({func, sub, blockId, day}){
+export default function Block({block, func}){
     
     const[active, setActive] = useState(false);
 
-    let block = null;
+    let blockElem = null;
 
     function getRef(element){
         if(element !== null){
-            block = element;
+            blockElem = element;
         }
     }
 
     function onMouseDown(event){
-        if(event.buttons === 1){
-            if(block !== null)
-                block.removeEventListener('mousedown',onMouseDown);
-            func.createFloat(sub, day, blockId, {x: event.pageX, y: event.pageY});
-        } else if(event.buttons === 2){
-            func.openSubjectMenu({x: event.pageX, y: event.pageY}, day, blockId, true)
-        }
+
+        func.createFloat(block, {x: event.pageX, y: event.pageY});
+        blockElem.removeEventListener('mousedown',onMouseDown);
+        // if(event.buttons === 1){
+        //     if(block !== null)
+        //         block.removeEventListener('mousedown',onMouseDown);
+        //     func.createFloat(block, {x: event.pageX, y: event.pageY});
+        // } else if(event.buttons === 2){
+        //     func.openSubjectMenu({x: event.pageX, y: event.pageY}, day, blockId, true)
+        // }
         
     }
 
-    function openMenu(event){
-        if(event.buttons === 2)
-            func.openSubjectMenu({x: event.pageX, y: event.pageY}, day, blockId, false)
-    }
+    // function openMenu(event){
+    //     if(event.buttons === 2)
+    //         func.openSubjectMenu({x: event.pageX, y: event.pageY}, day, blockId, false)
+    // }
 
     useEffect(() => {
-        const obj = {
-            id: blockId,
-            subject: sub,
-            elem: block,
-            isActive: active,
-            setState: setActive,
-        };
-        func.addBlock(blockId, obj, day);
-
-        if(block !== null){
-            if(sub !== ""){
-                block.addEventListener('mousedown',onMouseDown);
-            } else {
-                block.addEventListener('mousedown',openMenu);
-            }
-        }
+        if(blockElem !== null)
+            blockElem.addEventListener('mousedown',onMouseDown);
+        // if(block !== null){
+        //     if(sub !== ""){
+        //         block.addEventListener('mousedown',onMouseDown);
+        //     } else {
+        //         block.addEventListener('mousedown',openMenu);
+        //     }
+        // }
 
         return () => {
-            block.removeEventListener('mousedown',onMouseDown);
-            block.removeEventListener('mousedown',openMenu);
+            blockElem.removeEventListener('mousedown',onMouseDown);
+        //     block.removeEventListener('mousedown',openMenu);
         }
     });
 
     return (
-        <div ref={getRef} className={active ? "block highlight" : "block"}>
-            {sub}
+        <div ref={getRef} day={block.day} slot={block.slot} id={block.id} className={block.isHigh ? "block highlight" : "block"}>
+            {block.subject}
         </div>
     );
 
